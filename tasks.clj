1b80694b9b566e18306cb694cc2668e187faa2ac
ninjudd <git@justinbalthrop.com>
2010-11-22 19:11:42 -0800

    run compile before jar, but do nothing if build/native exists

--- tasks.clj
+++ tasks.clj
@@ -28,34 +28,33 @@
               rpath "-Wl,-R,'$$ORIGIN'"]
           (ant Replace {:file (file src "Makefile") :token token :value (str token " " rpath)}))))
 
-(deftask compile-native #{clean}
-  (log (format "Compiling tokyocabinet for %s/%s" (os-name) (os-arch)))
-  (let [dest    (.getPath (file "build" "native"))
-        prefix  (str "--prefix=" dest)
-        classes (file "classes")
-        arch    (case (os-arch) "x86_64" "-m64" "x86" "-m32" "")]
-    (let [src (file "src" tokyocabinet)]
-      (ant ExecTask {:executable "./configure" :dir src :failonerror true}
-           (args prefix)
-           (env {"CFLAGS" arch "LDFLAGS" arch}))
-      (fix-install-path src "tokyocabinet")
-      (ant ExecTask {:executable "make" :dir src :failonerror true})
-      (ant ExecTask {:executable "make" :dir src :failonerror true}
-           (args "install")))
-    (let [cflags (format " %s -I%s/include -L%s/lib " arch dest dest)
-          src    (file "src" tokyocabinet-java)]
-      (ant ExecTask {:executable "./configure" :dir src :failonerror true}
-           (args prefix)
-           (env {"JAVA_HOME" (System/getProperty "java.home") "CFLAGS" cflags}))
-      (let [token "\nCFLAGS ="] ; hack because configure doesn't set CFLAGS correctly in Makefile
-        (ant Replace {:file (file src "Makefile") :token token :value (str token " " cflags)}))
-      (fix-install-path src "jtokyocabinet")
-      (ant ExecTask {:executable "make" :dir src :failonerror true})
-      (ant ExecTask {:executable "make" :dir src :failonerror true}
-           (args "install")))
-    (ant Copy {:todir classes}
-         (add-zipfileset {:src (str dest "/lib/tokyocabinet.jar") :includes "**/*.class"}))
-    (rm (file dest "lib/tokyocabinet.jar"))
-    (rmdir (file dest "lib/pkgconfig"))))
-
-(remove-dep! jar compile)
+(deftask compile-native
+  (when-not (.exists (file "build" "native"))
+    (log (format "Compiling tokyocabinet for %s/%s" (os-name) (os-arch)))
+    (let [dest    (.getPath (file "build" "native"))
+          prefix  (str "--prefix=" dest)
+          classes (file "classes")
+          arch    (case (os-arch) "x86_64" "-m64" "x86" "-m32" "")]
+      (let [src (file "src" tokyocabinet)]
+        (ant ExecTask {:executable "./configure" :dir src :failonerror true}
+             (args prefix)
+             (env {"CFLAGS" arch "LDFLAGS" arch}))
+        (fix-install-path src "tokyocabinet")
+        (ant ExecTask {:executable "make" :dir src :failonerror true})
+        (ant ExecTask {:executable "make" :dir src :failonerror true}
+             (args "install")))
+      (let [cflags (format " %s -I%s/include -L%s/lib " arch dest dest)
+            src    (file "src" tokyocabinet-java)]
+        (ant ExecTask {:executable "./configure" :dir src :failonerror true}
+             (args prefix)
+             (env {"JAVA_HOME" (System/getProperty "java.home") "CFLAGS" cflags}))
+        (let [token "\nCFLAGS ="]  ; hack because configure doesn't set CFLAGS correctly in Makefile
+          (ant Replace {:file (file src "Makefile") :token token :value (str token " " cflags)}))
+        (fix-install-path src "jtokyocabinet")
+        (ant ExecTask {:executable "make" :dir src :failonerror true})
+        (ant ExecTask {:executable "make" :dir src :failonerror true}
+             (args "install")))
+      (ant Copy {:todir classes}
+           (add-zipfileset {:src (str dest "/lib/tokyocabinet.jar") :includes "**/*.class"}))
+      (rm (file dest "lib/tokyocabinet.jar"))
+      (rmdir (file dest "lib/pkgconfig")))))
