0ea6bf817cc921c8e21ae4a031fb432b2a91ead1
ninjudd <git@justinbalthrop.com>
2011-03-17 19:59:59 -0700

    cake tasks cleanup

--- tasks.clj
+++ tasks.clj
@@ -1,13 +1,12 @@
 (ns tasks
   (:use [cake.core :only [deftask]]
         [cake.file :only [file rm rmdir]]
-        [bake.core :only [log]]
-        [uncle.core :only [ant args env add-zipfileset]]
-        [cake.utils :only [os-name os-arch]])
+        [bake.core :only [log os-name os-arch]]
+        [uncle.core :only [ant args env add-zipfileset]])
   (:import [org.apache.tools.ant.taskdefs Copy ExecTask Replace]))
 
-(def tokyocabinet      "tokyocabinet-1.4.45")
-(def tokyocabinet-java "tokyocabinet-java-1.23")
+(def tokyocabinet      "tokyocabinet")
+(def tokyocabinet-java "tokyocabinet-java")
 
 (defn clean [srcdir]
   (when (.exists (file srcdir "Makefile"))
@@ -29,32 +28,35 @@
           (ant Replace {:file (file src "Makefile") :token token :value (str token " " rpath)}))))
 
 (deftask compile-native
-  (when-not (.exists (file "build" "native"))
-    (log (format "Compiling tokyocabinet for %s/%s" (os-name) (os-arch)))
-    (let [dest    (.getPath (file "build" "native"))
-          prefix  (str "--prefix=" dest)
-          classes (file "classes")
-          arch    (case (os-arch) "x86_64" "-m64" "x86" "-m32" "")]
-      (let [src (file "src" tokyocabinet)]
-        (ant ExecTask {:executable "./configure" :dir src :failonerror true}
-             (args prefix)
-             (env {"CFLAGS" arch "LDFLAGS" arch}))
-        (fix-install-path src "tokyocabinet")
-        (ant ExecTask {:executable "make" :dir src :failonerror true})
-        (ant ExecTask {:executable "make" :dir src :failonerror true}
-             (args "install")))
-      (let [cflags (format " %s -I%s/include -L%s/lib " arch dest dest)
-            src    (file "src" tokyocabinet-java)]
-        (ant ExecTask {:executable "./configure" :dir src :failonerror true}
-             (args prefix)
-             (env {"JAVA_HOME" (System/getProperty "java.home") "CFLAGS" cflags}))
-        (let [token "\nCFLAGS ="]  ; hack because configure doesn't set CFLAGS correctly in Makefile
-          (ant Replace {:file (file src "Makefile") :token token :value (str token " " cflags)}))
-        (fix-install-path src "jtokyocabinet")
-        (ant ExecTask {:executable "make" :dir src :failonerror true})
-        (ant ExecTask {:executable "make" :dir src :failonerror true}
-             (args "install")))
-      (ant Copy {:todir classes}
-           (add-zipfileset {:src (str dest "/lib/tokyocabinet.jar") :includes "**/*.class"}))
-      (rm (file dest "lib/tokyocabinet.jar"))
-      (rmdir (file dest "lib/pkgconfig")))))
+  (let [os-name (os-name)
+        os-arch (os-arch)
+        dest    (file "build" "native" os-name os-arch)]
+    (when-not (.exists dest)
+      (log (format "Compiling tokyocabinet for %s/%s" os-name os-arch))
+      (let [dest    (.getPath dest)
+            prefix  (str "--prefix=" dest)
+            classes (file "classes")
+            arch    (case os-arch "x86_64" "-m64" "x86" "-m32" "")]
+        (let [src (file "src" tokyocabinet)]
+          (ant ExecTask {:executable "./configure" :dir src :failonerror true}
+               (args prefix)
+               (env {"CFLAGS" arch "LDFLAGS" arch}))
+          (fix-install-path src "tokyocabinet")
+          (ant ExecTask {:executable "make" :dir src :failonerror true})
+          (ant ExecTask {:executable "make" :dir src :failonerror true}
+               (args "install")))
+        (let [cflags (format " %s -I%s/include -L%s/lib " arch dest dest)
+              src    (file "src" tokyocabinet-java)]
+          (ant ExecTask {:executable "./configure" :dir src :failonerror true}
+               (args prefix)
+               (env {"JAVA_HOME" (System/getProperty "java.home") "CFLAGS" cflags}))
+          (let [token "\nCFLAGS ="] ; hack because configure doesn't set CFLAGS correctly in Makefile
+            (ant Replace {:file (file src "Makefile") :token token :value (str token " " cflags)}))
+          (fix-install-path src "jtokyocabinet")
+          (ant ExecTask {:executable "make" :dir src :failonerror true})
+          (ant ExecTask {:executable "make" :dir src :failonerror true}
+               (args "install")))
+        (ant Copy {:todir classes}
+             (add-zipfileset {:src (str dest "/lib/tokyocabinet.jar") :includes "**/*.class"}))
+        (rm (file dest "lib/tokyocabinet.jar"))
+        (rmdir (file dest "lib/pkgconfig"))))))
