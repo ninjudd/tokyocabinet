da001f3eeeab8cf69ef306cae013df68d26ca0ba
Anthony Grimes <i@raynes.me>
2012-01-18 19:27:21 -0600

    Update fs.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject tokyocabinet "1.24.1"
   :description "native tokyo cabinet libraries"
   :dependencies [[clojure "1.3.0"]]
-  :dev-dependencies [[fs "1.1.1"]])
+  :dev-dependencies [[fs "1.1.2"]])
