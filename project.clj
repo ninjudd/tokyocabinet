53d41aba9e70a4ff9a090a2e47d3dfd18381a812
Anthony Grimes <i@raynes.me>
2012-01-18 19:03:05 -0600

    Added fs dev-dep.

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
-(defproject tokyocabinet "1.24.2"
+(defproject tokyocabinet "1.24.1"
   :description "native tokyo cabinet libraries"
-  :dependencies [[clojure "1.3.0"]])
+  :dependencies [[clojure "1.3.0"]]
+  :dev-dependencies [[fs "1.1.1"]])
