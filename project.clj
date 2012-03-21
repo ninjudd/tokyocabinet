b4b9e728d935b268a92f06d0b7e20230e716435a
Anthony Grimes <i@raynes.me>
2012-03-20 20:29:21 -0500

    Update conch.

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :min-lein-version "2.0.0"
   :description "native tokyo cabinet libraries"
   :dependencies [[fs "1.1.2"]
-                 [conch "0.2.0"]]
+                 [conch "0.2.2"]]
   :java-source-paths ["src"]
   :eval-in-leiningen true)
