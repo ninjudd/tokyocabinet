d2c145c196a34dc5efa15e63c206c6761cce8d26
Justin Balthrop <git@justinbalthrop.com>
2012-03-22 13:23:36 -0700

    fixes #3 - rework compile-native to support building multiple architectures

--- src/leiningen/compile_native.clj
+++ src/leiningen/compile_native.clj
@@ -1,5 +1,5 @@
 (ns leiningen.compile-native
-  (:use [leiningen.core.eval :only [get-os get-arch]]
+  (:use [leiningen.core.eval :only [get-os]]
         [clojure.java.io :only [copy file]])
   (:require [fs.core :as fs]
             [clojure.string :as string]
@@ -9,16 +9,16 @@
 (defn replace-text [f re text]
   (spit f (string/replace (slurp f) re text)))
 
-(defn fix-install-path [src lib]
-  (case (get-os)
-        :macosx
-        (replace-text (file src "Makefile")
-                      (str "-install_name $(LIBDIR)/lib" lib ".$(LIBVER).dylib")
-                      (str "-install_name @loader_path/lib" lib ".dylib"))
-        :linux
-        (let [token "\nLDFLAGS ="
-              rpath "-Wl,-R,'$$ORIGIN'"]
-          (replace-text (file src "Makefile") token (str token " " rpath)))))
+(defn fix-install-path [os-name src lib]
+  (case os-name
+    "macosx"
+    (replace-text (file src "Makefile")
+                  (str "-install_name $(LIBDIR)/lib" lib ".$(LIBVER).dylib")
+                  (str "-install_name @loader_path/lib" lib ".dylib"))
+    "linux"
+    (let [token "\nLDFLAGS ="
+          rpath "-Wl,-R,'$$ORIGIN'"]
+      (replace-text (file src "Makefile") token (str token " " rpath)))))
 
 ;; This was copied from Leiningen (and heavily modified). Probably want
 ;; to move this to fs.
@@ -30,38 +30,58 @@
             :when (pred filename)]
       (let [out-file (file out filename)]
         (.setCompressedSize zfile -1) ; some jars report size incorrectly
-        (.mkdirs (.getParentFile out-file)) 
+        (.mkdirs (.getParentFile out-file))
         (copy (.getInputStream zip zfile) out-file)))))
 
+(defn configure [src target env]
+  (fs/chmod "+x" (file src "configure"))
+  (sh/stream-to-out
+   (sh/proc "./configure" (str "--prefix=" target)
+            :dir src :env env)
+   :out))
+
+(defn make [src & params]
+  (sh/stream-to-out
+   (apply sh/proc "make"
+          (concat params [:dir src]))
+   :out))
+
+(defn make-native [target os arch-flag]
+  (let [src (file target "tokyocabinet-native")]
+    (configure src target {"CFLAGS" arch-flag "LDFLAGS" arch-flag})
+    (fix-install-path os src "tokyocabinet-native")
+    (make src "-j")
+    (make src "install")
+    (fs/delete-dir (file target "lib/pkgconfig"))))
+
+(defn make-java [target os arch-flag]
+  (let [src    (file target "tokyocabinet")
+        cflags (format " %s -I%s/include -L%s/lib " arch-flag target target)]
+    (configure src target {"JAVA_HOME" (or (System/getenv "JAVA_HOME")
+                                           (System/getProperty "java.home"))
+                           "CFLAGS" cflags})
+    (let [token "\nCFLAGS ="] ; hack because configure doesn't set CFLAGS correctly in Makefile
+      (replace-text (file src "Makefile") token (str token " " cflags)))
+    (fix-install-path os src "jtokyocabinet")
+    (make src)
+    (make src "install")
+    (copy-entries (str target "/lib/tokyocabinet.jar") (file "classes")
+                  #(.endsWith (str %) ".class"))
+    (fs/delete (file target "lib/tokyocabinet.jar"))))
+
+(defn compile-tokyo [target os arch-flag]
+  ;; copy source so we can compile for multiple architectures
+  (fs/copy-dir (file "src") target)
+  (make-native target os arch-flag)
+  (make-java   target os arch-flag))
+
 (defn compile-native [project]
-  (let [os-name (name (get-os))
-        os-arch (name (get-arch))
-        dest    (fs/file (:target-path project) "native" os-name os-arch)]
-    (when-not (.exists dest)
-      (println (format "Compiling tokyocabinet for %s/%s" os-name os-arch))
-      (let [prefix  (str "--prefix=" dest)
-            classes (file "classes")
-            arch    (case os-arch :x86_64 "-m64" :x86 "-m32" "")
-            src     (file "src" "tokyocabinet-native")]
-        (sh/stream-to-out
-          (sh/proc "./configure" prefix :dir src :env {"CFLAGS" arch "LDFLAGS" arch})
-          :out)
-        (fix-install-path src "tokyocabinet-native")
-        (sh/stream-to-out (sh/proc "make" "-j" :dir src) :out)
-        (sh/stream-to-out (sh/proc "make" "install" :dir src):out )
-        (let [cflags (format " %s -I%s/include -L%s/lib " arch dest dest)
-              src    (file "src" "tokyocabinet")]
-          (sh/stream-to-out (sh/proc "./configure" prefix
-                                     :dir src
-                                     :env {"JAVA_HOME" (or (System/getenv "JAVA_HOME")
-                                                           (System/getProperty "java.home"))
-                                           "CFLAGS" cflags})
-                            :out)
-          (let [token "\nCFLAGS ="] ; hack because configure doesn't set CFLAGS correctly in Makefile
-            (replace-text (file src "Makefile") token (str token " " cflags)))
-          (fix-install-path src "jtokyocabinet")
-          (sh/stream-to-out (sh/proc "make" :dir src) :out)
-          (sh/stream-to-out (sh/proc "make" "install" :dir src) :out))
-        (copy-entries (str dest "/lib/tokyocabinet.jar") classes #(.endsWith (str %) ".class"))
-        (fs/delete (file dest "lib/tokyocabinet.jar"))
-        (fs/delete-dir (file dest "lib/pkgconfig"))))))
+  (doseq [[arch arch-flag] {"x86_64" "-m64", "x86" "-m32"}]
+    (let [os     (name (get-os))
+          target (file (:target-path project) os arch)
+          dest   (file "resources" "native" os arch)]
+      (println (format "Compiling tokyocabinet for %s/%s" os arch))
+      (compile-tokyo target os arch-flag)
+      ;; we can't cross-compile yet, so copy native libs into resources/ so we can commit them
+      (fs/delete-dir dest)
+      (fs/copy-dir (file target "lib") dest))))
