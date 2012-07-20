1d190a13b2ed0f92f470603269050048fc086f66
Anthony Grimes <i@raynes.me>
2012-07-20 16:51:33 -0500

    Add license info.

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
 (defproject tokyocabinet "1.24.4"
+  :url "https://github.com/flatland/tokyocabinet"
+  :license {:name "GNU LESSER GENERAL PUBLIC LICENSE - Version 2.1"
+            :url "http://www.gnu.org/licenses/lgpl.html"}
   :min-lein-version "2.0.0"
   :description "native tokyo cabinet libraries"
   :dependencies [[fs "1.1.2"]
