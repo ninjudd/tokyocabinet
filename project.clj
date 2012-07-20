325b538d49ea6bc0a2a1f2edcc1c758a7f3d22dc
Anthony Grimes <i@raynes.me>
2012-07-20 16:49:50 -0500

    Prevent java compilation and don't include src/ in the jar.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject tokyocabinet "1.24.3"
+(defproject tokyocabinet "1.24.4"
   :min-lein-version "2.0.0"
   :description "native tokyo cabinet libraries"
   :dependencies [[fs "1.1.2"]
                  [conch "0.2.4"]]
-  :java-source-paths ["src"]
+  :source-paths ["somewhereovertherainbow"]
   :eval-in-leiningen true)
